59a78b76642e1ef570397ed36488f5fb71e5342a
Codeword <saldatejared@gmail.com>
2022-09-07 15:21:26 -0700

    added questions to generator

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,87 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer")
+const fs = require("fs")
 
-// TODO: Create an array of questions for user input
+
+inquirer.prompt(
+    [
+        {
+            type:"input",
+            message:"What is the title of your project",
+            name:"title"
+            
+            
+        },
+
+        {
+            type:"input",
+            message:"How do you install you app?",
+            name:"Instructions"
+ 
+
+        },
+
+        {
+            type:"input",
+            message:"Instructions to be follow?",
+            name:"Instructions"
+      
+
+        },
+
+        {
+            type:"input",
+            message:"Any Credits?",
+            name:"Instilation"
+            
+
+        },
+        {
+            type:"input",
+            message:"How do you use your app?",
+            name:"usage"
+            
+
+        },
+        {
+            // list licesne
+            type:"list",
+            message:"What license did you use?",
+            name:"License",
+            choices: ["The MIT Licesne", "The Apache licesne", "The GPL Licesne", 'N/A']
+            
+
+        },
+        {
+        type:"input",
+        message:"Github Username:",
+        name:"git",
+               
+
+    },
+    {
+        type:"input",
+        message:"email:",
+        name:"email",
+        
+        }
+    ]
+).then(answers =>{
+    console.log(answers.title)
+    fs.writeFileSync("./dist/README.md",
+`
+# ${answers.title}
+`
+`
+## ${answers.instalation}
+`
+    )
+})
+
+
+
+
+
 const questions = [];
 
 // TODO: Create a function to write README file
